e9e2b1764c2deb80a69c419c043322933333ad1c
yeonily <wluds9791@gmail.com>
2022-08-02 22:58:55 +0900

    2022-08-02

---
+++ myZone/src/aug02/AM.java
@@ -0,0 +1,117 @@
+package aug02;
+
+import java.util.Scanner;
+
+public class AM {
+	//메소드는 메소드 밖에 클래스 안에서 선언
+	//{} 있으면 선언 / 없으면 사용
+	//매개변수는 여러 개 가능
+	
+	//두 정수의 곱셈 메소드 선언
+	int mul(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+
+	public static void main(String[] args) {
+
+		//메소드 사용시, 메소드의 소속을 먼저 알려주기
+		AM am = new AM();
+		
+		am.mul(2, 4);//값
+		//리턴타입 ==  void : 값x
+		//리턴타입 != void : 값
+		System.out.println(am.mul(2, 4));
+		
+		
+//      사용자에게 입력받은 문자열 중 소문자는 모두 대문자로,
+//      대문자는 모두 소문자로 변경한다.
+		//length(), charAt()
+		
+		//입력 메소드 임포트
+		Scanner sc = new Scanner(System.in);
+		//입력받은 문자열을 담을 변수, 결과를 담을 변수, 문자열 입력 메세지 변수 선언
+		String str = null, result = "", msg = "문자 입력 : ";  
+		
+		//문자열 입력 메세지 출력
+		System.out.print(msg);
+		//입력된 문자열 변수에 대입
+		str = sc.nextLine();
+		
+		//0부터 문자열의 길이만큼 반복문
+		//문자열의 i번째 문자를 캐릭터 타입 변수에 대입
+		//캐릭터 타입 변수가 대문자일 경우
+		//소문자로 변환하여 결과변수에 더하고 대입
+		//캐릭터 타입 변수가 소문자일 경우
+		//대문자로 변환하여 결과변수에 더하고 대입
+		//그외의 문자일 경우
+		//그대로 결과변수에 더하고 대입
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if(c >= 65 && c <= 90) {
+				result += (char)(c + 32);
+			}else if(c >= 96 && c <= 122) {
+				result += (char)(c - 32);
+			}else {
+				result += c;
+			}
+		}
+		
+		
+		//결과 출력
+		System.out.println(result);
+		
+		
+		
+		
+//		 정수를 한글로 변경
+//	      예) 1024
+//	        -> 일공이사 
+			//charAt()
+		
+		//입력할 정수를 담을 변수 선언(int 타입)
+		int num = 0;
+		//정수 입력 메세지, 한글숫자가 담긴 변수, 결과값 선언
+		String message = "숫자 입력 : ", hangle = "공일이삼사오육칠팔구", res = "";
+		//입력할 정수를 담을 변수 선언(String 타입)
+		String number = null;
+		
+		//int 방법
+		//정수 입력 메시지 출력
+		System.out.print(message);
+		//입력값을 변수에 대입
+		num = sc.nextInt();
+		
+		//변수가 0이 될 때까지 계속 반복, 0이면 탈출
+		//정수를 10으로 나눈 나머지 값을 한글숫자로 추출하고 기존 결과를 더하여 결과변수에 대입
+		//정수를 10으로 나누고 대입
+		while(num != 0) {
+			res = hangle.charAt(num % 10) + res;
+			num /= 10;
+		}
+		
+		//결과 출력
+		System.out.println(res);
+		
+		//String 방법
+		//정수 입력 메세지 출력
+		System.out.println(message);
+		//입력값을 변수에 대입
+		number = sc.nextLine();
+		
+		//0부터 문자 길이만큼 반복
+		//i번째 인덱스의 문자를 추출하여 아스키코드표를 참고하여 해당 숫자를 만든 뒤, 한글숫자문자 추출하여 결과 변수에 대입
+		for (int i = 0; i < number.length(); i++) {
+			res += hangle.charAt(number.charAt(i) - 48);
+		}		
+		
+		//결과 출력
+		System.out.println(res);
+		
+		
+		//원하는 인덱스의 문자 추출 >> charAt(0)
+		//원하는 문자 조회 및 검색 >> indexOf("a")
+		
+	}
+
+}
---
+++ myZone/src/aug02/PM.java
@@ -0,0 +1,102 @@
+package aug02;
+
+public class PM {
+	
+//  1~10까지 println()으로 출력하는 메소드
+	//메소드 선언
+	//1~10까지 반복문
+	//1~10까지 출력
+	void print1to10() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i+1);
+		}
+	}
+	
+//  "홍길동"을 n번 println()으로 출력하는 메소드
+	//메소드 선언, 매개변수 필요(n번)
+	//0부터 n번까지 반복문
+	//"홍길동"출력
+	void printHong(int count) {
+		for (int i = 0; i < count; i++) {
+			System.out.println("홍길동");
+		}
+	}
+	
+	
+//  이름을 n번 println()으로 출력하는 메소드
+	//메소드 선언, 매개변수 필요(이름, n번)
+	//0부터 n번까지 반복문
+	//이름 출력
+	void printName(String name, int count) {
+		for (int i = 0; i < count; i++) {
+			System.out.println(name);
+		}
+	}
+	
+//  세 정수의 뺄셈을 해주는 메소드
+	//메소드 선언, 매개변수(세 정수)
+	//세 정수의 뺄셈값 리턴
+	int sub(int num1, int num2, int num3) {
+		return num1-num2-num3;
+	}
+	
+	
+//  두 정수의 나눗셈을 해주는 메소드(몫과 나머지 둘 다)
+	//메소드 선언, 매개변수(두 정수)
+	//리턴 값이 2개이므로 배열에 담기, 배열 선언
+	int[] divide(int num1, int num2) {
+		int[] res = null;
+		
+		if(num2 != 0) {
+			res = new int[2];
+			
+			res[0] = num1 / num2;
+			res[1] = num1 % num2;
+			
+		}
+		return res;
+	}
+	
+	//분모가 0이 아니라면
+	//배열 칸수 생성
+	
+	//배열 0칸에 몫 대입
+	//배열 1칸에 나머지 대입
+	
+	//결과값 리턴
+	
+	
+	public static void main(String[] args) {
+		//메소드의 소속 알려주기
+		PM pm = new PM();
+		//선언한 메소드 사용하기
+		pm.print1to10();
+		pm.printHong(2);
+		pm.printName("나비", 3);
+		System.out.println(pm.sub(6, 1, 1));
+		
+		//divide 메소드 리턴값은 int배열이므로 배열 선언해서 리턴값 대입 
+		int[] res = pm.divide(20, 0);
+		//메세지 변수 선언(초기화 시, 대입할 땐 null / 누적 연산할 땐 "")
+		String msg = null;
+		//메세지는 리턴값이 null일 때 "0으로 나눌 수 없습니다", null이 아닐 때는 몫과 나머지 값 대입
+		msg = res == null ? "0으로 나눌 수 없습니다" : "몫 : "+res[0]+"\n나머지 : "+res[1];
+		
+		//메시지 출력
+		System.out.println(msg);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
